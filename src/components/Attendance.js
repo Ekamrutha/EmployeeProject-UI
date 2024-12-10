7f47a6f01da8b9273c44b08c6078ae572b5a0fe2
Ekamrutha Challagulla <challagullaekamrutha@gmail.com>
2024-12-10 14:57:43 +0530

    Attendance tab final chnages

--- src/components/Attendance.js
+++ src/components/Attendance.js
@@ -5,7 +5,6 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import moment from 'moment';
 import './Attendance.css';
-import WeekNumbers from 'react-calendar/dist/cjs/MonthView/WeekNumbers';
 
 const Attendance = () => {
 
@@ -17,6 +16,7 @@ const Attendance = () => {
   }, [empID])
   const [date, setDate] = useState('');
   const [selectedButton, setSelectedButton] = useState('Day');
+  const [index, setIndex] = useState(1);
   const attendance = attendanceData.filter(obj => obj.date === moment(date).format('YYYY-MM-DD'));
   const attendanceDisplay = attendance.length === 0 ? [{
     captured : "-",
@@ -28,10 +28,23 @@ const Attendance = () => {
   const minDate = new Date(moment().subtract(3, 'months'));
 
   const [dateRange, setDateRange] = useState([]);
-  const firstDate = moment(dateRange?.[0]).format('YYYY-MM-DD');
-  const lastDate = moment(dateRange?.[1]).format('YYYY-MM-DD');
-
+  const firstDate = dateRange.length ? moment(dateRange?.[0]).format('YYYY-MM-DD') : '';
+  const lastDate = dateRange.length ? moment(dateRange?.[1]).format('YYYY-MM-DD') : '';
+  
   const dateRangeAttendance = attendanceData.filter(obj => obj.date <=  moment(lastDate).format('YYYY-MM-DD') && obj.date>= moment(firstDate).format('YYYY-MM-DD'));
+
+  const handleNextClick = () => {
+   if(index<dateRangeAttendance.length){
+    setIndex(index+1);
+   }
+  }
+
+  const handlePrevClick = () => {
+   if(index>1){
+    setIndex(index-1);
+   }
+  }
+
   const selectedAttendance = (selectedButton) => {
       if(selectedButton === 'Day'){
       return date!=='' ? 
@@ -48,15 +61,22 @@ const Attendance = () => {
       </div>
       }
       else if (selectedButton === 'Date Range'){
-        return firstDate!=='' && lastDate!=='' ? 
-        dateRangeAttendance.map(obj => <div className='attendanceContainer'><div className='attendance'>
-        <div className='display'>Attendance : <div className={moment(obj.date).format('dddd') === 'Saturday' || moment(obj.date).format('dddd') === 'Sunday' ? '' : obj.captured === 'Y' ? 'green' : 'red'}>{moment(obj.date).format('dddd') === 'Saturday' || moment(obj.date).format('dddd') === 'Sunday' ? 'Weekly Off' : ''}</div></div>
-        <p>Date : {moment(obj.date).format('Do MMMM YYYY')}</p>
-        <p>Day : {moment(obj.date).format('dddd')}</p>
-        <p>In Time : {obj.captured === 'N' ? '-' : obj.in_time}</p>
-        <p>Out Time : {obj.captured === 'N' ? '-' : obj.out_time}</p>
-      </div></div>) : 
+        return (firstDate!=='' && lastDate!=='') ? 
+        (<div className='attendanceContainer'><div className='attendance'>
+        <div className='display'>Attendance : <div className={moment(dateRangeAttendance[index-1]['date']).format('dddd') === 'Saturday' || moment(dateRangeAttendance[index-1]['date']).format('dddd') === 'Sunday' ? '' : dateRangeAttendance[index-1]['captured']=== 'Y' ? 'green' : 'red'}>{moment(dateRangeAttendance[index-1]['date']).format('dddd') === 'Saturday' || moment(dateRangeAttendance[index-1]['date']).format('dddd') === 'Sunday' ? 'Weekly Off' : ''}</div></div>
+        <p>Date : {moment(dateRangeAttendance[index-1]['date']).format('Do MMMM YYYY')}</p>
+        <p>Day : {moment(dateRangeAttendance[index-1]['date']).format('dddd')}</p>
+        <p>In Time : {dateRangeAttendance[index-1]['captured'] === 'N' ? '-' : dateRangeAttendance[index-1]['in_time']}</p>
+        <p>Out Time : {dateRangeAttendance[index-1]['captured'] === 'N' ? '-' : dateRangeAttendance[index-1]['out_time']}</p>
+        <div className='buttonContainer1'>
+          <div className={index===1 ? 'disabledButton' : 'navigateButton'} onClick={handlePrevClick}>Prev</div>
+          <div className={index===dateRangeAttendance.length ? 'disabledButton' : 'navigateButton'} onClick={handleNextClick}>Next</div>
+        </div>
+      </div>
+      </div>) : 
+      <div className='attendanceContainer'>
          <div className='attendance'>Please select Date Range</div>
+      </div>
       }
   }
 
@@ -64,8 +84,8 @@ const Attendance = () => {
     <div className='main'>
       <div>
         <div className='buttonContainer'>
-            <div className={selectedButton==='Day' ? 'selectedButton' : 'attendanceButton'} onClick={() => setSelectedButton('Day')}>Day</div>
-            <div className={selectedButton==='Date Range' ? 'selectedButton' : 'attendanceButton'} onClick={() => setSelectedButton('Date Range')}>Date Range</div>
+            <div className={selectedButton==='Day' ? 'selectedButton' : 'attendanceButton'} onClick={() => {setSelectedButton('Day'); setDate('')}}>Day</div>
+            <div className={selectedButton==='Date Range' ? 'selectedButton' : 'attendanceButton'} onClick={() => {setSelectedButton('Date Range'); setDateRange([])}}>Date Range</div>
       </div>
       {selectedAttendance(selectedButton)}
       </div>
